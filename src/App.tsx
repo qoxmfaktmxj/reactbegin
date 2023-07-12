759ba9910cf8815ce0fb30529053839c9d6e3bd4
qoxmfaktmxj <qoxmfaktmxj@naver.com>
2023-07-12 21:49:37 +0900

    리액트추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let post = '강남우동맛집';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className="black-nav">
+            <h4 style={{color:"yellowgreen", fontSize:"20px"}}>회원요청목록</h4>
+        </div>
+        <div className="list">            
+            <h4>글제목</h4>
+            <p>7월 12일 발행</p>
+        </div>
+
     </div>
   );
 }
